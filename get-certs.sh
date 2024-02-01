a99d876fa4ffd5cdc721c59d421145b560f9ce4b
Myles Best <myles+git@empty.best>
2024-01-31 22:46:56 -0500

    let it be long running

--- get-certs.sh
+++ get-certs.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-set -ex
+set -x
 
 CONFIG_FILE="${CONFIG_FILE:-/etc/certbot/config.ini}"
 HOSTS_FILE="${HOSTS_FILE:-/etc/certbot/hosts.ini}"
+SLEEP_TIME="${SLEEP_TIME:-86400}" # once a day
 
 if [ -z "${EMAIL}" ]
 then
@@ -16,4 +17,6 @@ while read line; do
   HOSTS=$(echo -n "-d ${line}" | sed 's/,/ \-d /g')
   certbot certonly --non-interactive --agree-tos -m ${EMAIL} --dns-rfc2136 --dns-rfc2136-credentials ${CONFIG_FILE} ${HOSTS} 
 done < ${HOSTS_FILE}
+
+sleep ${SLEEP_TIME}
 
